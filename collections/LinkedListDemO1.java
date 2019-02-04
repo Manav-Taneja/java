e360a0522e7f348bf5ee58b3a8c45e5be21de797
manav <manav18csu121@ncuindia.edu>
2019-02-04 12:04:03 +0530

    collection folder is added

---
+++ collections/LinkedListDemO1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class LinkedListDemO1{
+	public static void main(String[] args)
+	{
+	LinkedList<String> ll=new LinkedList<String>();
+	System.out.println("initial size of ll:"+ll.size());
+	ll.add("C");
+	ll.add("A");
+	ll.add("E");
+	ll.add("D");
+	ll.add(1,"A2");
+	ll.addLast("z");
+	ll.addFirst("a");
+
+	System.out.println("initial size addition al:"+ll);
+
+	ll.remove("F");
+	ll.remove(2);
+	System.out.println("after deletion al:" +ll);
+	ll.removeFirst();
+	ll.removeLast();
+	System.out.println("after deletion of first and last al:"+ll);
+	String val=ll.get(2);
+	ll.set(2,"5");
+	System.out.print("changed list :" +ll);
+	}
+}
